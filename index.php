8650a711d231db33992ae5e1bbdc533bf1c2f3bc
AntonioSotoAS <arturoantoniomontejosoto@gmail.com>
2023-03-17 01:15:34 -0500

    avanzado navbar y inicio, falta icon rocket

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Finvoice</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"></script>
+  
+</head>
+
+<body>
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+      <a class="navbar-brand d-flex align-items-center" href="#">
+        <img src="/img/logoFinvoice.png" height="50px" width="50px" alt="Logo">
+        <h1 class="ms-2 mb-0" style="font-size: 50px;">finvoice</h1>
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <ul class="navbar-nav fs-5">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Inicio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Beneficios</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Testimonios</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">FAQs</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">LOGIN</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!--por el momento pondre br-->
+
+  <br>
+  <br><br><br>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <div class="text-center">
+          <img src="/img/inicioFinvoice.svg" alt="Imagen" class="img-fluid" />
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="text-center">
+          <h4>Solución Tecnológica para Factoring Digital</h4>
+        </div>
+        <div class="text-center">
+          <h1 style="font-size: 50px;"><strong>Anticipa el pago de<br> tus facturas de<br> manera simple y<br> rápida</strong></h1>
+        </div>
+        <div class="text-center">
+          <p style="font-size: 18px; margin-top: 40px;">
+            Nuestro proceso de financiamiento es muy sencillo. Un<br> chequeo rápido
+            y un poco de papeleo pueden hacer<br> llegar los fondos a tu empresa en
+            el mismo día.
+          </p>
+        </div>
+        <div class="text-center">
+          <button type="button" style="margin-top: 40px;" class="btn btn-dark btn-lg"><i class=""></i>Contáctanos</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+</body>
+
+</html>
